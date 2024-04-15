2279b973eed7ff4529a348bfd427f57329c54510
ssb <sbexclusive9@gmail.com>
2024-04-15 16:38:24 +0900

    react_study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header";
+import DayList from './component/DayList';
+import Day from './component/Day';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<DayList/>}></Route>
+          <Route path="/day/:day" element={<Day/>}></Route>
+          <Route path="*" element={<EmptyPage/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
